7d4008eab3c8247b88f933a03d3ccc6fa478f2ea
danny p <danny@dannys-MacBook-Pro.local>
2022-03-10 19:01:49 -0500

    added pngs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,10 @@ import './home.css'
 import smash4 from '../../images/smash-4.png'
 import ultimate from '../../images/ultimate.png'
 import kirby from '../../images/kirby.png'
+import mario from '../../images/mario.webp'
+import link from '../../images/link.png'
+import sheik from '../../images/sheik.png'
+import charizard from '../../images/charizard.png'
 
 
 
@@ -24,12 +28,19 @@ const Home = () => {
       
 
       <div className='right-cont'>
+
+      <img className="kirby" src={kirby} />
+
       <div className="img-cont">
       <img className="img-right" src={ultimate} />
       <p className='ult-p'>SUPER SMASH BROS. ULTIMATE</p>
       </div>
 
       <img className="kirby" src={kirby} />
+      <img className="mario" src={mario} />
+      <img className="link" src={link} />
+      <img className="sheik" src={sheik} />
+      <img className="charizard" src={charizard} />
 
       </div>
 
